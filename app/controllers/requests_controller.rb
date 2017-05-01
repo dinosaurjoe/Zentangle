f6ed4c5d2bea5b51a260e8ef6b997af3af19d911
wenqingkevin ma <wenqing.kevin@gmail.com>
2017-05-01 16:18:37 +0200

    added the methods for request but not finished, paths still unclear atm

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,16 +1,35 @@
 class RequestsController < ApplicationController
   def index
+    @request = Request.all
   end
 
   def show
+    @request = Request.find(params[:id])
   end
 
   def new
+    @request = Request.new()
   end
 
   def create
+    @request = Request.new(request_params)
+    @project = Project.find(params[:project_id])
+    @request.user = current_user
+    @request.project = @project
+    if @request.save
+      redirect_to dashboard_path
+    else
+      redirect_to new_project_booking_path
+      # path unclear
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def request_params
+    params.require(:request).permit(:user_id, :role_id)
   end
 end
