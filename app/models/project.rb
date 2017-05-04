562f68771572ab117944c7e0d420609c88afbcae
Joe Schafer <joeschaferastula@gmail.com>
2017-05-04 11:35:01 +0200

    added filters for index

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,45 @@
 class Project < ApplicationRecord
+
+  CATEGORIES = {
+    art: [
+      "ceramics", "conceptual art", "digital art", "illustration", "installations",
+    "mixed media", "painting", "performance art", "public art", "sculpture", "textiles",
+    "video art", "printmaking"
+    ],
+    design: [
+      "architecture", "civic design", "graphic design", "interactive design",
+    "product design", "typography", "industrial design", "interior design"
+    ],
+    fashion: [
+      "accessories", "apparel", "childrenswear", "couture", "footwear",
+    "jewelry", "pet fashion"
+    ],
+    film: [
+      "action", "animation", "comedy", "documentary", "drama", "experimental", "family",
+    "fantasy", "horror", "music videos", "narrative film", "romance", "science fiction",
+    "short"
+    ],
+    journalism: [
+      "audio", "photo", "video", "print", "web"
+    ],
+    music: [
+      "blues", "chiptune", "classical music", "comedy", "country & folk", "electronic music",
+    "faith", "hip-hop", "indie rock", "jazz", "kids", "latin", "metal", "pop", "punk",
+    "r&b", "rock", "world music"
+    ],
+    photography: [
+      "animals", "fine art", "nature", "people", "places"
+    ],
+    technology: [
+      "3d printing", "apps", "diy electronics", "fabrication tools",
+    "gadgets", "hardware", "robots", "software", "game design", "sound", "web"
+    ],
+    theater: [
+      "comedy", "experimental", "immersive", "musical", "plays",
+    "set design", "dance"
+    ]
+  }
+
   belongs_to :user
   alias_attribute :owner, :user
   has_many :roles
@@ -9,30 +50,4 @@ class Project < ApplicationRecord
   mount_uploader :picture, PhotoUploader
 
 # check validations
-
-  class << self
-    # Project.categories
-    def categories
-      {art: ["ceramics", "conceptual art", "digital art", "illustration", "installations",
-        "mixed media", "painting", "performance art", "public art", "sculpture", "textiles",
-        "video art", "printmaking"],
-      design: [ "architecture", "civic design", "graphic design", "interactive design",
-        "product design", "typography", "industrial design", "interior design"],
-      fashion: ["accessories", "apparel", "childrenswear", "couture", "footwear",
-        "jewelry", "pet fashion"],
-      film: ["action", "animation", "comedy", "documentary", "drama", "experimental", "family",
-        "fantasy", "horror", "music videos", "narrative film", "romance", "science fiction",
-        "short"],
-      journalism: ["audio", "photo", "video", "print", "web"],
-      music: ["blues", "chiptune", "classical music", "comedy", "country & folk", "electronic music",
-        "faith", "hip-hop", "indie rock", "jazz", "kids", "latin", "metal", "pop", "punk",
-        "r&b", "rock", "world music"],
-      photography: ["animals", "fine art", "nature", "people", "places"],
-      technology: ["3d printing", "apps", "diy electronics", "fabrication tools",
-        "gadgets", "hardware", "robots", "software", "game design", "sound", "web"],
-      theater: ["comedy", "experimental", "immersive", "musical", "plays",
-        "set design", "dance"]}
-    end
-
-  end
 end
