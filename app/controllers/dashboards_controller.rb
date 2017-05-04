f6ad67e7a3a05c50c7db92ec41eba3ba8c319f57
Alex Espinosa <espinosa_alex@yahoo.com>
2017-05-04 18:10:02 +0200

    added status info

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -25,6 +25,15 @@ class DashboardsController < ApplicationController
       end
     end
   end
+
+  def request_status(request)
+    if request.user_confirm && request.owner_confirm
+      return "joined"
+    elsif request.user_confirm.nil? || request.owner_confirm.nil?
+      return "pending"
+    elsif !request.user_confirm || !request.owner_confirm
+      return "declined"
+  end
 end
 
 
