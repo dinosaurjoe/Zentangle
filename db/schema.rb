f3020b1e465e8117ca6397d7332a8d16f755bdcc
Alex Espinosa <espinosa_alex@yahoo.com>
2017-05-04 16:20:51 +0200

    everything working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170503125637) do
+ActiveRecord::Schema.define(version: 20170504130324) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20170503125637) do
     t.string   "title"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.boolean  "status"
     t.index ["project_id"], name: "index_roles_on_project_id", using: :btree
   end
 
