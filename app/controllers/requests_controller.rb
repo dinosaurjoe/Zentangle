e6b6e67aeab7c1bb1550cb3d1d15a9164e35c553
Izabel Lima <belica18@gmail.com>
2017-05-11 14:41:44 +0200

    fixed

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,6 +1,7 @@
 class RequestsController < ApplicationController
   before_action :set_role, only: []
   before_action :set_request, only: [:show, :edit, :update, :destroy]
+  skip_after_action :verify_authorized
   def index
     authorize @request
     @request = Request.all
